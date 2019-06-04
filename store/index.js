e21190b44e0022a7f24a7369817d895d75fa670f
acidjazz <acidjazz@gmail.com>
2019-06-04 14:36:35 -0500

    🔧 store conversion

--- store/index.js
+++ store/index.js
@@ -1,24 +1,16 @@
-import Vuex from 'vuex'
-
-const store = () => new Vuex.Store({
-
-  state: {
+export const state = () => ({
     modal: false,
     menu: 'black',
-  },
-
-  mutations: {
-    menuColor (state, color) {
-      state.menu = color
-    },
-
-    modalOn (state) {
-      state.modal = true
-    },
-    modalOff (state) {
-      state.modal = false
-    },
-  },
 })
 
-export default store
+export const mutations = {
+  menuColor (state, color) {
+    state.menu = color
+  },
+  modalOn (state) {
+    state.modal = true
+  },
+  modalOff (state) {
+    state.modal = false
+  },
+}
