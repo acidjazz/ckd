22ef3d9fcb30d231ac5a76d1712882e2f7b0dc9c
kevin olson <acidjazz@gmail.com>
2021-12-16 15:06:54 -0600

    💚 zip code change

--- components/CAddress.vue
+++ components/CAddress.vue
@@ -2,5 +2,5 @@
 a.address.is-c1s(href="https://goo.gl/maps/bn37jKFZqnJU4P6c9",target="_new")
   .copy.is-c1s 3450 sacramento st, #519
   .copy.is-c1s san francisco, ca
-  .copy.is-c1s 94115
+  .copy.is-c1s 94118
 </template>
